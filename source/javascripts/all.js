0bf1194838291cfe27ae2afb8a3352666853af4a
Mariusz Ciesla <mariusz@dotmariusz.net>
2015-03-18 20:06:30 +0100

    Final

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 //= require components/webcomponentsjs/webcomponents.min
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  var $button = document.querySelector('.button');
+  var $pages = document.querySelector('core-pages');
+
+  $button.addEventListener('click', function(){
+    var nextSelected = parseInt($pages.selected) + 1;
+    var totalPages = $pages.items.length;
+
+    if (nextSelected < totalPages)
+      $pages.selected = nextSelected;
+    else
+      $pages.selected = 0;
+  });
+
+});
